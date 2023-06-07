9ce5692ef98ff803d286263147babbd765b33d70
AbizerS10 <abizersusnerwala786@gmail.com>
2023-06-07 17:37:30 +0530

    status bar

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./store/index";
 import { PersistGate } from "redux-persist/integration/react";
 import ToDo from "./components/ToDo";
+import { StatusBar } from "expo-status-bar";
+
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <StatusBar translucent={true} />
         <ToDo />
       </PersistGate>
     </Provider>
